a4f0c20bd7c2d8aeb102e40061ff83351b006eda
Cauã <92538361+raulino20@users.noreply.github.com>
2023-12-27 09:56:36 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,5 +3,88 @@ function reload(){
 }
 
 function Close(){ 
-alert('Ação Indisponivel no momento, foi mal ...  Ass :CRS ')
+
+    window.close()
+
 }
+
+function Change(){
+
+const CarouselGTA = document.getElementById('NextCard')
+
+const Text = document.getElementById('TituloPag')
+
+const ContGamer = document.getElementById('ContainerGamer')
+
+const ContReal = document.getElementById('ContainerReal')
+
+const Header = document.getElementById('headerInc')
+
+const Background = document.getElementById('backChange')
+
+const Container = document.getElementById('C02')
+
+const CarouselReal = document.getElementById('ProCard')
+
+const TitleGTA = document.getElementById('TitleGTA')
+
+const TitleReal = document.getElementById('TitleReal')
+
+const textGTA = document.getElementById('TextGTA')
+
+const TextReal = document.getElementById('TextReal')
+
+const ImgReal = document.getElementById('ImgReal')
+
+const ImgGTA = document.getElementById('ImgGTA')
+
+
+CarouselGTA.style.display = 'none'
+CarouselReal.style.display = 'block'
+CarouselReal.style.width = '80%'
+CarouselReal.style.height = '100%'
+CarouselReal.style.textAlign = 'center'
+CarouselReal.style.boxShadow = '0.3rem 0.3rem 1rem green'
+CarouselReal.style.margin = '0.5rem auto'
+CarouselReal.style.backgroundSize = 'cover'
+
+
+
+Background.style.backgroundImage = 'url("imagens/miami-night.jpg")'
+Background.style.backgroundPosition = 'center'
+Background.style.backgroundSize = 'cover'
+
+
+
+Container.style.textAlign = 'center'
+Container.style.margin = 'auto'
+Container.style.textAlign = 'center'
+Container.style.width = '100%'
+Container.style.height = 'auto'
+Container.style.backgroundSize = 'cover'
+
+
+Header.style.backgroundImage = 'linear-gradient(black,blue)'
+
+Text.style.textDecorationColor = 'purple'
+
+
+
+ContGamer.style.display = 'none'
+ContReal.style.display = 'flex'
+
+TitleGTA.style.display = 'none'
+TitleReal.style.display = 'flex'
+
+textGTA.style.display = 'none'
+TextReal.style.display = 'flex'
+
+ImgGTA.style.display = 'none'
+ImgReal.style.display = 'flex'
+
+
+
+
+}
+
+
